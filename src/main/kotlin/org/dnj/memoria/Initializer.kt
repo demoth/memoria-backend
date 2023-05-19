57e012ae6c8b1c98b7aae40aa20ac4df5c1eb026
demoth <bubnov.d.e@gmail.com>
2023-05-19 14:14:14 +0200

    1.0.5: cleanup initializer

--- src/main/kotlin/org/dnj/memoria/Initializer.kt
+++ src/main/kotlin/org/dnj/memoria/Initializer.kt
@@ -10,16 +10,8 @@ class Initializer(
     val itemRepository: ItemRepository
 ): CommandLineRunner {
     override fun run(vararg args: String?) {
-        val demoth = getOrCreateUser("demoth")
-        val denolia = getOrCreateUser("denolia")
-
-        itemRepository.save(Item("Task", "Позвонить в страховую", Status.Backlog, Priority.High, demoth, demoth, null, "Не знаю ещё зачем"))
-        itemRepository.save(Item("Task", "Пополнить баланс", Status.Todo, Priority.Medium, demoth))
-        itemRepository.save(Item("Task", "Помыть кухню", Status.Backlog, Priority.Medium, denolia))
-        itemRepository.save(Item("Task", "Поиграть в PoE", Status.Backlog, Priority.High, denolia))
-        itemRepository.save(Item("Epic", "Написать свой ноушен", Status.InProgress, Priority.High, demoth, denolia))
-        itemRepository.save(Item("Task", "Сходить в магазин", Status.Done, Priority.Low, demoth))
-        itemRepository.findAll().forEach { println(it) }
+        getOrCreateUser("demoth")
+        getOrCreateUser("denolia")
     }
 
     private fun getOrCreateUser(userName: String): User {
