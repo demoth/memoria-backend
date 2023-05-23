e9958dd795f3d8c1c1a48ed61c69a1f981aa44cf
demoth <bubnov.d.e@gmail.com>
2023-05-23 21:59:21 +0200

    1.0.7: add /user endpoint to update passwords

---
+++ src/main/kotlin/org/dnj/memoria/controllers/UserController.kt
@@ -0,0 +1,41 @@
+package org.dnj.memoria.controllers
+
+import org.dnj.memoria.UserDto
+import org.dnj.memoria.UserRepository
+import org.dnj.memoria.service.AuthService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class ChangePasswordRequest(
+    val id: String,
+    val currentPassword: String,
+    val newPassword: String
+)
+
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val userRepository: UserRepository,
+    private val authService: AuthService
+) {
+    
+    @PostMapping
+    fun update(
+        @RequestHeader("Authentication") token: String,
+        @RequestBody request: ChangePasswordRequest
+    ): ResponseEntity<UserDto> {
+        val user = authService.validateToken(token)
+        if (user.id != request.id ||
+            user.password != request.currentPassword) {
+            return ResponseEntity.badRequest().build()
+        }
+
+        user.password = request.newPassword
+        return ResponseEntity.ok(userRepository.save(user).toDto())
+    }
+
+}
